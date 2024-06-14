9f0cf6963cf26862fe29112ec42c1d77696514d0
Michael Gee <michaelgee221@gmail.com>
2024-06-14 10:49:36 -0400

    replace lodash with communitils

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { UserProfile } from './examples/UserProfile';
-import isEmpty from 'lodash/isEmpty';
+import { isEmpty } from 'communitils';
 import { profile } from './examples/api/data';
 
 import './main.css';
